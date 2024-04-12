00875f4db130e186a9864909b99a735a74cc55a8
ndikrp <andhikarizky36@gmail.com>
2024-04-13 00:03:26 +0700

    add error handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+module.exports = {
+    errorHandler(err, req, res, next) {
+        res.status(err.status || 500).json({
+            status: "error",
+            message: err.message || err
+        });
+    },
+};
